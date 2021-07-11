cdddeae39ea87a718b34a4c213b998d8201c1367
Soumya0111 <87131897+Soumya0111@users.noreply.github.com>
2021-07-11 13:21:28 +0530

    Add files via upload

---
+++ ManytoMany/UserDetails.java
@@ -0,0 +1,65 @@
+package ManytoMany;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import OnetoOne.Vehicle;
+
+	@Entity
+	@Table(name="userdetails")	
+	public class UserDetails
+	{
+		@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+		private int userid;
+		private String username;
+
+		@ManyToMany
+		private Collection <Vehicle> vehicle=new ArrayList<Vehicle>();
+		
+		
+		
+		public Collection<Vehicle> getVehicle() {
+			return vehicle;
+		}
+		public void setVehicle(Collection<Vehicle> vehicle) {
+			this.vehicle = vehicle;
+		}
+		public int getUserid() {
+			return userid;
+		}
+		public void setUserid(int userid) {
+			this.userid = userid;
+		}
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public UserDetails(int userid, String username) {
+			super();
+			this.userid = userid;
+			this.username = username;
+		}
+		@Override
+		public String toString() {
+			return "UserDetails [userid=" + userid + ", username=" + username + "]";
+		}
+		public UserDetails() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+
+	}
+
+
---
+++ ManytoMany/Vehicle.java
@@ -0,0 +1,62 @@
+package ManytoMany;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import OnetoMany.UserDetails;
+
+@Entity
+@Table (name="vehicledetails")
+public class Vehicle
+{
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int vehicleid;
+	private String vehiclename;
+	
+
+	@ManyToMany(mappedBy="vehicle")
+	private Collection <UserDetails> user =new ArrayList();
+	
+	
+	public Collection<UserDetails> getUser() {
+		return user;
+	}
+	public void setUser(Collection<UserDetails> user) {
+		this.user = user;
+	}
+	public int getVehicleid() {
+		return vehicleid;
+	}
+	public void setVehicleid(int vehicleid) {
+		this.vehicleid = vehicleid;
+	}
+	public String getVehiclename() {
+		return vehiclename;
+	}
+	public void setVehiclename(String vehiclename) {
+		this.vehiclename = vehiclename;
+	}
+	@Override
+	public String toString() {
+		return "Vehicle [vehicleid=" + vehicleid + ", vehiclename=" + vehiclename + "]";
+	}
+	public Vehicle(int vehicleid, String vehiclename) {
+		super();
+		this.vehicleid = vehicleid;
+		this.vehiclename = vehiclename;
+	}
+	public Vehicle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
